7f4b3187e452a54d2c2ed172d034ee5b9ba75e81
Saurav Mallik <sauravmallik786@gmail.com>
2023-01-10 16:48:48 +0530

    ddaa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import Home from './Components/home/Home';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <main className='main'>
+      <Home />
+    </main>
     </div>
   );
 }
