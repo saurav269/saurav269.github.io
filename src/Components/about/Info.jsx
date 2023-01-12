9458264e2366228dfa99e4f19fadbe704d8fda23
Saurav Mallik <sauravmallik786@gmail.com>
2023-01-13 01:37:12 +0530

    bi

---
+++ src/Components/about/Info.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+ <div className='about__info grid'>
+    <div className='about__box'>
+    <i class='bx bx-briefcase-alt about__icon'></i>
+
+    <h3 className='about__title'>Completed</h3>
+    <span className='about__subtitle'>6+ Projects</span>
+    </div>
+
+    <div className='about__box'>
+    <i class='bx bx-support about__icon'></i>
+    <h3 className='about__title'>Support</h3>
+    <span className='about__subtitle'>Online 24/7</span>
+    </div>
+</div>
+  )
+}
+
+export default Info
