771183a96879b2249e5fe1fe4fb0169424d6ff06
Saurav Mallik <sauravmallik786@gmail.com>
2023-01-17 18:57:15 +0530

    jj

--- src/Components/work/Works.jsx
+++ src/Components/work/Works.jsx
@@ -5,15 +5,44 @@ import Work1 from '../../assets/work1.png'
 import Work2 from '../../assets/work2.png'
 import Work3 from '../../assets/work3.png'
 import Work4 from '../../assets/work4.png'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Works = () => {
-  return (
+
+    const[item, setItem] = useState({name : "all"})
+    const[projects, setProjects] = useState([]);
+    const[active, setActive] = useState(0);
+
+    useEffect(() => {
+        if(item.name === 'all'){
+            setProjects(projectsData)
+        }else{
+            const newProjects = projectsData.filter((project) =>{
+                return project.category === item.name;
+            });
+            setProjects(newProjects)
+        }
+    },[item]);
+
+    const handleClick=(e,index)=>{
+          setItem({name : e.target.textContent})
+          setActive(index)
+    }
+
+return (
 <div>
      <div className='work__filters'>
         {projectsNav.map((item, index) => {
-
-            return <span className='work__item'>
-                {item.name}
+            return <span 
+            onClick={(e) => {
+               handleClick(e,index)
+            }}
+            className={`${active === index ? 'active-work' : ""}
+            work__item`}
+            key={index}
+            >
+            {item.name}
             </span>
         })}  
     </div>
